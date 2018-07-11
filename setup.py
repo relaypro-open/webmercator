889abfe7393aab330e2e8aaa88f1a03afb31eaa1
Dillon Stadther <dlstadther@gmail.com>
2018-07-11 09:23:40 -0400

    Update to version 0.1.2 (fbo PyPi)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def read(fname):
 
 setup(
     name='webmercator',
-    version='0.1.1',
+    version='0.1.2',
     description='Mercator coordinate manipulation for tile systems',
     author='Dayne Batten and Dillon Stadther',
     url='https://github.com/republicwireless-open/webmercator',
